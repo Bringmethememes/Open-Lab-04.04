3139f18fc9161730df2c78e969866503e4dadd4c
PC <PC@ANDREJKOVPOITA>
2020-10-23 23:04:40 +0200

    Snazil so sa o rim nevysiel mi trim.

--- Open-Lab-04.04/PhoneNumbers.cs
+++ Open-Lab-04.04/PhoneNumbers.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection.Metadata.Ecma335;
 
 namespace Open_Lab_04._04
 {
@@ -6,7 +7,27 @@ namespace Open_Lab_04._04
     {
         public string FormatPhoneNumber(int[] numbers)
         {
-            throw new NotImplementedException();
-        }
+            int[] pleb = new int[numbers.Length - 7];
+            for(int i = 0; i < pleb.Length; i++)
+            {
+                pleb[i] = numbers[i];
+            }
+            string bruh = string.Join("",(pleb));
+            string bruhplus = "(" + bruh + ")" + " " + numbers[3] + numbers[4] + numbers[5] + "-" + numbers[6] + numbers[7] + numbers[8] + numbers[9];
+            return bruhplus;
+            
+
+
+
+
+
+
+
+
+
+        } 
+
+
+       
     }
 }
